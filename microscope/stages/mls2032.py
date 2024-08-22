52e18ce5d929df2ce20b8f5c8f34872e4b3d6ce3
Rowan Nicholls <rowan.nicholls@dtc.ox.ac.uk>
2024-08-22 15:58:47 +0100

    Updating stage code

--- microscope/stages/mls2032.py
+++ microscope/stages/mls2032.py
@@ -13,19 +13,37 @@ class MLS2032StageAxis(microscope.abc.StageAxis):
     """
 
     def __init__(
-        self, controller, axis: str, simulated=False, verbose=False
+        self, controller: microscope.abc.Controller, axis: str,
+        limits=[-10000, 10000], simulated=False, verbose=False
     ) -> None:
         if verbose:
-            print('Initialising stage axis', axis)
+            print(f"Initialising the stage's {axis} axis")
         super().__init__()
         self.controller = controller
         self.axis = axis
+        self._limits = limits
+        assert limits[0] < limits[1], \
+            f'The minimum limit ({limits[0]}) cannot be larger or equal ' + \
+            f'to the maximum limit ({limits[1]})'
+        self.min_limit = limits[0]
+        self.max_limit = limits[1]
+        self._position = limits[0] + (limits[1] - limits[0]) / 2
+        self._home_position = limits[0] + (limits[1] - limits[0]) / 2
         self.simulated = simulated
 
-        # Not a good solution as min/max are used to build the stage map in
-        # Mosaic, etc
-        self.min_limit = 0.0
-        self.max_limit = 100000.0
+    def move_by(self, delta: float, verbose=False) -> None:
+        """Move the device by the specified distance."""
+        if verbose:
+            print(f"Moving the stage's {self.axis} axis by {delta}")
+        relative = True
+        self.move(delta, relative, verbose=verbose)
+
+    def move_to(self, pos: float, verbose=False) -> None:
+        """Move the device to the specified position."""
+        if verbose:
+            print(f"Moving the stage's {self.axis} axis to {pos}")
+        relative = False
+        self.move(pos, relative, verbose=verbose)
 
     def convert_to_mm(self, value, unit):
         """Convert a given value from specified units to millimetres (mm)."""
@@ -48,44 +66,67 @@ class MLS2032StageAxis(microscope.abc.StageAxis):
 
         return converted_value
 
-    def move_by(self, delta: float, verbose=False) -> None:
-        """Move by the specified distance."""
-        if verbose:
-            print(f'Moving stage axis {self.axis} by {delta}')
-        relative = True
-        self.move(delta, relative)
-
-    def move_to(self, pos: float, verbose=False) -> None:
-        """Move to the specified position."""
-        if verbose:
-            print(f'Moving stage axis {self.axis} to {pos}')
-        relative = False
-        self.move(pos, relative)
-
     def move(self, target_pos, relative, units='mm', verbose=False):
         """Move the stage axis to a specified end position."""
         target_mm_pos = self.convert_to_mm(target_pos, units)
-        self.move_mm(target_mm_pos, relative, verbose)
+        self.move_mm(target_mm_pos, relative=relative, verbose=verbose)
 
-    def move_mm(self, target_pos, relative=False, verbose=False):
-        """Moves the stage axis to a specified end position."""
-        if self.controller:
+    def move_mm(self, value, relative=False, verbose=False):
+        """Move the stage axis to a specified end position."""
+        # Check against the axis's limits
+        if relative:
+            if value + self._position > self.max_limit:
+                value = self.max_limit - self._position
+            if value + self._position < self.min_limit:
+                value = self.min_limit + self._position
+        else:
+            if value > self.max_limit:
+                value = self.max_limit
+            if value < self.min_limit:
+                value = self.min_limit
+        # Update the user
+        if verbose:
+            if relative:
+                print(f"Moving the stage's {self.axis} axis by {value} mm")
+            else:
+                print(f"Moving the stage's {self.axis} axis to {value} mm")
+        # Move the axis
+        if self.simulated:
+            if relative:
+                self._position += value
+            else:
+                self._position = value
+        else:
             self.controller.moveTo(
-                self, target_pos, relative=relative, verbose=verbose
+                self, value, relative=relative, verbose=verbose
             )
+            # self.controller.move_to(delta, self.name)
 
     @property
     def position(self) -> float:
+        """Get the position of this axis."""
         if self.controller:
             if self.controller.is_busy():
                 self.controller.wait_until_idle()
-            return float(self.controller.get_absolute_position(self._axis))
+            return float(self.controller.get_absolute_position(self.axis))
+            # return self.controller.position(self.axis)
+        elif self.simulated:
+            return self._position
 
     @property
     def limits(self) -> microscope.AxisLimits:
+        """Get the limits of the position of the axis."""
         return microscope.AxisLimits(
             lower=self.min_limit, upper=self.max_limit
         )
+
+    def get_home_position(self) -> float:
+        """Get the home position of this axis."""
+        return self._home_position
+
+    def set_home_position(self, position):
+        """Set the home position of this axis."""
+        self._home_position = position
 
 
 class MLS2032Stage(microscope.abc.Stage):
@@ -94,49 +135,40 @@ class MLS2032Stage(microscope.abc.Stage):
     """
 
     def __init__(
-        self, config, controller=None, simulated=False, verbose=False
+        self, controller: microscope.abc.Controller, axes: list,
+        simulated=False, verbose=False
     ):
         """Constructor method."""
         # Initialize the base class (Stage), which also handles Device
         super().__init__()
-
-        self.stage_name = config.get('name', 'XY Stage')
-
         self.controller = controller
-        if self.controller is not None:
-            self.controller.__init__(config)
-        else:
-            print('A controller is not present.')
-            print('The stage is initialising without a controller.')
+        self.initialize()
+
+        if verbose:
+            stage_name = ''.join(axes).upper()
+            print(f'Initialising the {stage_name} stage')
 
         self.simulated = simulated
         if self.simulated:
-            print('This is a simulated stage.')
-            self.homed = None
-        else:
-            self.homed = self.get_homed_status(self, verbose=verbose)
+            print('This is a simulated stage')
 
-        # Define the stage axes
-        if verbose:
-            print('Defining the axes')
-        # Define the axes
+        # Define the stage's axes
         _axes = {}
-        for axis in ['x', 'y']:
+        for axis in axes:
             _axes[axis] = MLS2032StageAxis(
-                controller, axis, simulated, verbose
+                controller, axis, simulated=simulated, verbose=verbose
             )
         self._axes = _axes
 
-    @property
-    def axes(self) -> typing.Mapping[str, microscope.abc.StageAxis]:
-        return self._axes
+        # Check if the stage is homed
+        self.homed = self.get_homed_status(verbose=verbose)
 
     def initialise(
         self, home=True, force_home=False, max_vel=None, acc=None,
         verbose=False
     ):
         """
-        Initialise the XY stage.
+        Initialise an MLS203-2 stage.
 
         Parameters
         ----------
@@ -162,7 +194,7 @@ class MLS2032Stage(microscope.abc.Stage):
         self, channel=None, max_vel=None, acc=None, verbose=False
     ):
         """
-        Set the velocity and acceleration parameters of the XY stage.
+        Set the velocity and acceleration parameters of an MLS203-2 stage.
 
         Parameters
         ----------
@@ -222,24 +254,22 @@ class MLS2032Stage(microscope.abc.Stage):
         return converted_value
 
     def move_by(self, delta, verbose=False):
-        """Move by the specified distance."""
+        """Move an MLS203-2 stage by the specified distance."""
         if verbose:
-            print('Moving by', delta)
-        target_pos = (delta['x'], delta['y'])
+            print('Moving the stage by', delta)
         relative = True
-        self.move(target_pos, relative)
+        self.move(delta, relative, verbose=verbose)
 
     def move_to(self, position, verbose=False):
-        """Move to the specified position."""
+        """Move an MLS203-2 stage to the specified position."""
         if verbose:
-            print('Moving to', position)
-        target_pos = (position['x'], position['y'])
+            print('Moving the stage to', position)
         relative = False
-        self.move(target_pos, relative)
+        self.move(position, relative, verbose=verbose)
 
-    def move(self, target_pos, relative, units='mm', verbose=False):
+    def move(self, values, relative, units='mm', verbose=False):
         """
-        Move the XY stage to a specified end position.
+        Move an MLS203-2 stage to a specified end position.
 
         Parameters
         ----------
@@ -252,20 +282,27 @@ class MLS2032Stage(microscope.abc.Stage):
         verbose : bool, default False
             If True, enables verbose output during the move operation.
         """
-        target_mm_pos = self.convert_to_mm(target_pos, units)
-        self.move_mm(target_mm_pos, relative, verbose)
+        if verbose:
+            if relative:
+                print('Moving the stage by', values)
+            else:
+                print('Moving the stage to', values)
+        values_mm = {}
+        for key, value in values.items():
+            values_mm[key] = self.convert_to_mm(value, units)
+        self.move_mm(values_mm, relative=relative, verbose=verbose)
 
     def move_mm(
-        self, target_pos, channel=None, relative=False, max_vel=None, acc=None,
+        self, values, channel=None, relative=False, max_vel=None, acc=None,
         permanent=False, verbose=False
     ):
         """
-        Moves the XY stage to a specified end position.
+        Moves an MLS203-2 stage to a specified end position.
 
         Parameters
         ----------
-        target_pos : list or tuple
-            A tuple specifying the target X and Y positions.
+        values : list or tuple
+            A tuple specifying the target X, Y and Z (or a subset) positions.
         channel : list or tuple or None, default None
             List of channel names or numbers. If None, moves all channels.
         relative : bool, default False
@@ -279,16 +316,30 @@ class MLS2032Stage(microscope.abc.Stage):
         verbose : bool, default False
             Whether to print additional information.
         """
-        if self.controller:
+        if self.simulated:
+            for axis in self._axes.values():
+                name = axis.axis
+                value = values[name]
+                if verbose:
+                    if relative:
+                        print(f"Moving the stage's {name} axis by {value} mm")
+                    else:
+                        print(f"Moving the stage's {name} axis to {value} mm")
+                axis.move_mm(value, relative=relative, verbose=verbose)
+        else:
             self.controller.moveTo(
-                self, target_pos=target_pos, channel=channel,
+                self, target_pos=values, channel=channel,
                 relative=relative, max_vel=max_vel, acc=acc,
                 permanent=permanent, verbose=verbose
             )
 
+    @property
+    def axes(self) -> typing.Mapping[str, microscope.abc.StageAxis]:
+        return self._axes
+
     def home(self, force_home=False, verbose=False):
         """
-        Home the XY stage.
+        Home an MLS203-2 stage.
 
         Parameters
         ----------
@@ -299,56 +350,72 @@ class MLS2032Stage(microscope.abc.Stage):
             If True, enables verbose output during the homing operation.
         """
         if verbose:
-            print('Homing the stage.')
-        if self.controller:
+            print('Homing the stage')
+        if self.simulated:
+            for axis in self._axes.values():
+                axis.move_to(axis.get_home_position())
+        else:
             self.controller.home_channels(
                 self, channel=None, force=force_home, verbose=verbose
             )
-        self.homed = True
+        self.homed = self.get_homed_status(verbose=False)
 
     def get_homed_status(self, verbose=False):
-        """Check if the stage is homed."""
+        """Check if an MLS203-2 stage is homed."""
         if verbose:
-            print('Checking if the stage is homed.')
+            print('Checking if the stage is homed')
 
         # If this is a simulated stage
+        homed = []
         if self.simulated:
-            if verbose:
-                if self.homed:
-                    print('The stage is homed.')
-                else:
-                    print('The stage is not homed.')
-            return self.homed
+            for axis in self._axes.values():
+                homed.append(axis.position == axis.get_home_position())
+            if all(homed):
+                if verbose:
+                    print('The stage is homed')
+                return True
+            else:
+                if verbose:
+                    print('The stage is not homed')
+                return False
 
-        # If this is note a simulated stage
+        # If this is not a simulated stage
         homed = []
         for idx in self.controller.channels:
             # Check if all channels are homed
             homed.append(self.channels[idx].Status.IsHomed)
         if all(homed):
             if verbose:
-                print('The stage is homed.')
+                print('The stage is homed')
             return True
         else:
             if verbose:
-                print('The stage is not homed.')
+                print('The stage is not homed')
             return False
 
-    def get_position(self):
+    def get_position(self, verbose=False):
         """
-        Get the current position of the XY stage.
+        Get the current position of an MLS203-2 stage.
 
         Returns
         -------
         tuple
-            The current X and Y position of the stage.
+            The current position of the stage.
         """
-        if self.controller:
+        if verbose:
+            print('Getting the current position of the stage')
+
+        positions = []
+        if self.simulated:
+            for axis in self._axes.values():
+                positions.append(axis.position)
+            return positions
+        else:
             self.controller.get_position(self)
 
     def close(self, force, verbose):
         """
-        Cleans up and releases resources of the XY stage.
+        Cleans up and releases resources of an MLS203-2 stage.
 
         Parameters
         ----------
@@ -361,8 +428,9 @@ class MLS2032Stage(microscope.abc.Stage):
         if self.controller:
             self.controller.finish(self, verbose=verbose)
 
-    def _do_enable(self) -> bool:
-        print('Enabling the device.')
+    def _do_enable(self, verbose=False) -> bool:
+        if verbose:
+            print('Enabling the device')
         # Before a device can moved, it first needs to establish a reference to
         # the home position. We won't be able to move unless we home it first.
         if not self.homed:
@@ -372,52 +440,63 @@ class MLS2032Stage(microscope.abc.Stage):
 
     def may_move_on_enable(self, verbose=False) -> bool:
         if verbose:
-            print('Checking if the stage may move if it is enabled.')
-
-        may_move = not self.homed
-
-        if verbose:
-            if may_move:
-                print('The stage may move when enabled.')
-            else:
-                print('The stage will not move when enabled.')
-
-        return may_move
+            print('Checking if the stage may move if it is enabled')
+        return not self.homed
 
     def _do_shutdown(self) -> None:
-        print('Shutting down the MLS203-2 stage.')
+        print('Shutting down the MLS203-2 stage')
 
 
 if __name__ == '__main__':
     print('Testing mls2032.py')
 
-    config = {
-        'name': 'XY Stage'
-    }
+    controller = None
+    axes = ['x', 'y']
+    stage = MLS2032Stage(controller, axes, simulated=True, verbose=True)
 
-    stage = MLS2032Stage(config, simulated=True, verbose=True)
-    print(stage.stage_name)
     # Check if enabling the device will cause it to move
     print(stage.may_move_on_enable(verbose=True))
     stage.enable()
     stage.home(verbose=True)
     print(stage.get_homed_status(verbose=True))
+    print('')
 
     # Move operations
     stage.move_to({'x': 42.0, 'y': -5.1}, verbose=True)
+    print(stage.get_position(verbose=True))
+    print('')
     stage.move_by({'x': -5.3, 'y': 14.6}, verbose=True)
+    print(stage.get_position(verbose=True))
+    print('')
+
+    # Home the stage
+    print(stage.get_position(verbose=True))
+    stage.get_homed_status(verbose=True)
+    stage.home(verbose=True)
+    print(stage.get_position(verbose=True))
+    stage.get_homed_status(verbose=True)
+    print('')
 
     # Individual axes
     x_axis = stage.axes['x']
     y_axis = stage.axes['y']
     x_axis.move_to(42.0, verbose=True)
     y_axis.move_by(-5.3, verbose=True)
+    y_axis.move_by(1.2, verbose=True)
+    print(stage.get_position(verbose=True))
+    print('')
 
-    # Moves x axis to the its upper limit:
+    # Move the x-axis to its upper limit
     x_axis.move_to(x_axis.limits.upper, verbose=True)
+    print(stage.get_position(verbose=True))
+    print('')
 
-    # The same as above since the move operations are clipped to
-    # the axes limits automatically.
+    # Move the x-axis beyond its upper limit
+    # (this is the same as above because the move operations are clipped to
+    # the axes' limits automatically)
     import math
     x_axis.move_to(math.inf, verbose=True)
-    x_axis.move_by(math.inf, verbose=True)
+    print(stage.get_position(verbose=True))
+    y_axis.move_by(math.inf, verbose=True)
+    print(stage.get_position(verbose=True))
+    print('')
